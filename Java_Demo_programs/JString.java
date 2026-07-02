0ad53c25eab8352648360c8650dd66484e09e6a5
Sudip-Barman <sudipbarmab61@gmail.com>
2026-07-02 22:07:00 +0530

    adding the demo files

---
+++ Java_Demo_programs/JString.java
@@ -0,0 +1,5 @@
+public class JString{
+	public static void main(String[] args){
+		System.out.println(args);
+	}
+}
---
+++ Java_Demo_programs/Test.java
@@ -0,0 +1,65 @@
+public class A{
+
+
+
+	{
+		System.out.println("hii");
+	}
+
+	static{
+			System.out.println("Hello");
+	}
+
+	public static void main(String biswajit[]){
+				System.out.println("hii kya karuchu");
+
+
+		/*final String name = "Sudip";
+		byte age = 18;
+		char grade = 'A';
+		System.out.println("My name is: " + name);
+		System.out.println("My age is: " + (age+3));
+		System.out.println("My grade is : " + grade);
+
+		// System.out.println(args[1]);*/
+
+		/*byte x = 10;
+		System.out.println("this is my x value : " + x);
+		int a = x;
+		System.out.println("This is my a value : " + a);
+		short z = (short) a;
+		System.out.println("This is my z value : " + z);
+		char p = 45;
+		System.out.println("This is my p ---- " + p);
+
+		double div = 30.0 / 9;
+		System.out.println("This is my division value " + div);*/
+
+		// byte a = 10;
+		// short b = 20;
+		// char c = 30;
+		// int d = 40;
+		// long e = 50l;
+		// float f = 6054.25655445f;
+		// double g = 70;
+		// String h = "I am a string";
+		// // This is the output section
+		// System.out.println("This is my byte datatype : " + a);
+		// System.out.println("This is my short datatype : " + b);
+		// System.out.println("This is my char datataype : " + c);
+		// System.out.println("This is my int datatype : " + d);
+		// System.out.println("This is my long datatype : " + e);
+		// System.out.println("This is my float datatype : " + f);
+		// System.out.println("This is my double datatype : " + g);
+		// System.out.println(h);
+
+		// // d++;
+		// d = ++d;
+		// System.out.println("This is my d after increment : " + d);
+
+
+
+
+	}
+}
+
